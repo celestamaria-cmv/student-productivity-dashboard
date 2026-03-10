acda8f56a4cb704caae1b3d3d94fb57cfe5e9e7b
Celesta Maria Varghese <celestavarghese35@gmail.com>
2026-03-10 12:09:14 +0100

    Added Notes page with CSS styling

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,9 +1,77 @@
+import { useState,useEffect } from "react";
+import "../styles/Notes.css";
+
 function Notes(){
-    return(
-    <div>
-      <h2>Notes Page</h2>
-    </div>
-);
+  const [notes, setNotes] = useState(() => {
+    const saved = localStorage.getItem("notes");
+    return saved ? JSON.parse(saved) : [];
+});
+  const[input,setInput]=useState("");
+
+useEffect(() => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}, [notes]);
+
+function addNote(){
+  const trimmedNote=input.trim();
+
+  if(trimmedNote !==""){
+    setNotes([...notes,trimmedNote])
+    setInput("");
+  }
+
 }
 
+function deleteNote(index){
+  setNotes(notes.filter((_, i) => i !== index));
+}
+
+return(
+  <div className="notes-container">
+
+    <h2 className="notes-title">Notes</h2>
+
+    <div className="notes-input-section">
+
+      <input
+        className="notes-input"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e)=>{
+          if(e.key === "Enter"){
+            addNote();
+          }
+        }}
+        placeholder="Write a note"
+      />
+
+      <button className="add-note-btn" onClick={addNote}>
+        Add
+      </button>
+
+    </div>
+
+    <ul className="notes-list">
+      {notes.map((note,index)=>(
+        <li className="note-item" key={index}>
+
+          <span className="note-text">
+            {note}
+          </span>
+
+          <button
+            className="delete-note-btn"
+            onClick={()=>deleteNote(index)}
+          >
+            Delete
+          </button>
+
+        </li>
+      ))}
+    </ul>
+
+  </div>
+);
+}
 export default Notes;
